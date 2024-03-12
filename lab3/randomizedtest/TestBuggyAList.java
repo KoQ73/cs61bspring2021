f5ddc221553d8b3ac3a0a0e454a201f47187123c
Kan <koqkelvin@gmail.com>
2024-03-12 16:17:29 +0800

    Finished lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,61 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        correct.addLast(5);
+        correct.addLast(10);
+        correct.addLast(15);
+
+        broken.addLast(5);
+        broken.addLast(10);
+        broken.addLast(15);
+
+        assertEquals(correct.size(), broken.size());
+
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                broken.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                int brokenSize = broken.size();
+                assertEquals(size, brokenSize);
+            }
+            else if (operationNumber == 2) {
+                int size = L.size();
+                if (size > 0) {
+                    int last = L.getLast();
+                    int brokenLast = broken.getLast();
+                    assertEquals(brokenLast, last);
+                }
+            }
+            else if (operationNumber == 3) {
+                int size = L.size();
+                if (size > 0) {
+                    int removed = L.removeLast();
+                    int brokenRemoved = broken.removeLast();
+                    assertEquals(brokenRemoved, removed);
+                }
+            }
+        }
+    }
 }
